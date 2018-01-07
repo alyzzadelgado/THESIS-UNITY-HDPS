bc57a6b97efd14d61c65051adac7da094611dcf1
Alyzza Delgado <alyzza_delgado@dlsu.edu.ph>
2018-01-07 14:31:46 +0800

    version1.0
    - first commit by AD
    - start screen links to welcome screen
    - welcome screen "No" button links to main (menu) screen
    - googleVR "compatible" or ready

---
+++ HDPS/Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeScene : MonoBehaviour {
+
+	/*
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	*/
+
+	public void FuncChangeScene(string sceneName)
+    {
+        Application.LoadLevel(sceneName);
+
+
+    }
+}
